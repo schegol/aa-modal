bee05804784fad3cb0805d16bc56463a1c99b3a9
schegol <yovladdi@yandex.ru>
2023-04-09 01:09:05 +0400

    more preps; expanding the demo

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,12 +1,16 @@
 $(function () {
-	let modalCallBtns = $('[aa-modal]');
+    $('[aa-modal]').aamodal();
 
-    // modalCallBtns.aamodal();
+    $('.aa-modal-demo__btn--alt').aamodal({
+        src: '/ajax/modal-3.html',
+        id: 'altModal',
+        class: 'aa-modal__body--alt',
+        closeBtn: $('.modal-content__button'), //or '.modal-content__button'
+        animation: 'fromTop',
+        animationDuration: 3000,
+        // animationTimingFunction: 'ease-in-out',
+        // onOpenStart: function(event, trigger) {
 
-    modalCallBtns.on('click', function(e) {
-        e.preventDefault();
-        let btn = $(this);
-
-        btn.aamodal();
+        // },
     });
 });
--- dist/aa-modal.js
+++ dist/aa-modal.js
@@ -12,23 +12,73 @@
     'use strict';
 
     let error = null,
-        style = '';
+        style = '',
+        styleTag = $('<style></style'),
+        modalIsLoading = false,
+        modalIsOpen = false,
+        defaultOpenTriggers = '[aa-modal]',
+        defaultCloseTriggers = '[aa-modal-close]';
 
-    $.fn.aamodal = function(options) {
-        let settings = $.extend({
-            //setting1: '1',
-            //setting2: '2',
-            //...
+    //default settings:
+    let defaultSettings = {
+        src: '',
+        id: '',
+        class: '',
+        closeBtn: '',
+        animation: 'none',
+        animationDuration: '1000',
+        animationTimingFunction: 'linear',
 
-            // callbacks:
-            onOpenStart: function() {},
-            onOpenEnd: function() {},
-            onCloseStart: function() {},
-            onCloseEnd: function() {},
-        }, options);
- 
-        return this.each(function() {
-            console.log($(this));
-        });
+        // callbacks:
+        onOpenStart: function() {},
+        onOpenEnd: function() {},
+        onCloseStart: function() {},
+        onCloseEnd: function() {},
+    }
+
+    //methods:
+    let methods = {
+        init : function(options) {
+            let settings = $.extend({}, defaultSettings, options);
+
+            return this.each(function() {
+                let trigger = $(this);
+
+                // console.log($(this));
+
+                // selector = getSelectorString(trigger);
+
+                // $(document).on('click', selector, function (e) {
+                //     openModal(settings);
+                // });
+                trigger.on('click', function (e) {
+                    // openModal(settings);
+                    console.log('clicked');
+                });
+            });
+        },
+        close: function() {
+            console.log('close method called');
+        }
+    };
+
+    //inner functions:
+    function getSelectorString(obj) {
+        
+    }
+
+    function openModal(settings) {
+        
+    }
+
+    $.fn.aamodal = function(optionsOrMethod) {
+        if (methods[optionsOrMethod]) {
+            return methods[optionsOrMethod].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof optionsOrMethod === 'object' || !optionsOrMethod) {
+            return methods.init.apply(this, arguments); // default to methods[init]
+        } else {
+            // $.error('Method ' +  optionsOrMethod + ' does not exist on AA Modal');
+            error = 'Unknown AA Modal method called';
+        }  
     };
 }));
